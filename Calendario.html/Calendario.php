553f2c4ebff80de07a29c379f0db474d46cb0fef
Freddy-4830A <freddy.ochoa8@misena.edu.co>
2023-06-29 19:05:15 -0500

    Elaboración de vista calendario y boton flotante de ayuda para letra

---
+++ Calendario.html/Calendario.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Calendario.css">
+    <title>Admisiones</title>
+    <div class="franja-superior"></div>
+
+    <body>
+        <div class="calendario">
+            <h1>CALENDARIO</h1>
+          </div>
+
+          <div>
+            <hr width="340px" size="3px" color="#fff176">
+          </div>
+
+<center>
+    <div id="centro"></div>
+        <section id="calendario">
+
+        <body>
+
+        </body>
+
+    
+<footer>
+</div>  
+<div class="franja-inferior"></div>
+</footer>        
