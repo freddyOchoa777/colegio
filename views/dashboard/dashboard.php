a9d009c23f15187664c30e58ad905da43e851be4
David Palacios <david95palacios@outlook.es>
2023-07-20 21:00:19 -0500

    add buttons to print or export data

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -3,12 +3,14 @@
 <head>
   <title>Dashboard Colegio</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <!-- CSS -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-  <link rel="stylesheet" href="assets/css/dashboard.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/dashboard.css">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 </head>
 <body>
--- views/dashboard/listPerson.php
+++ views/dashboard/listPerson.php
@@ -1,97 +1,120 @@
 <div class="content">
-  <div class="menu">
-    <div class="d-flex justify-content-between">
-      <span>Bienvenid@,
-        <?php echo ($email) ?>
-      </span>
-      <form method="POST" action="?controller=Home">
-        <button class="btn btn-danger">Salir</button>
-      </form>
+      <div class="menu">
+        <div class="d-flex justify-content-between">
+          <span>Bienvenid@, <?php echo ($email) ?></span>
+          <form method="POST" action="?controller=Home">
+            <button class="btn btn-danger">Salir</button>
+          </form>
+        </div>
+      </div>
+
+      <h1>Lista de Usuarios</h1>
+      <div class="table-container">
+        <table id="userTable" class="table">
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Apellidos</th>
+              <th>Documento</th>
+              <th>Correo</th>
+              <th>Rol</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($dataUsers as $dataUser): ?>
+              <tr>
+                <td><?php echo $dataUser->Nombres ?></td>
+                <td><?php echo $dataUser->Apellidos ?></td>
+                <td><?php echo $dataUser->documento ?></td>
+                <td><?php echo $dataUser->correo ?></td>
+                <td><?php echo $dataUser->Rol ?></td>
+                <td>
+                  <a class="btn btn-tbl-delete btn-xs" href="?controller=Home&method=editUser&id=<?php echo $dataUser->idPersona ?>"><i class="fa fa-edit "></i></a>
+                  <a class="btn btn-tbl-eliminar-ajax btn-xs" href="?controller=Home&method=delete&id=<?php echo $dataUser->idPersona ?>"><i class="fas fa-trash-alt"></i></a>
+                </td>
+              </tr>
+            <?php endforeach ?>
+          </tbody>
+        </table>
+      </div>
     </div>
+    <!-- Fin de contenido -->
   </div>
 
-  <h1>Lista de Usuarios</h1>
-  <div class="table-container">
-    <table id="userTable">
-      <thead>
-        <tr>
-          <th>Nombres</th>
-          <th>Apellidos</th>
-          <th>Documento</th>
-          <th>Correo</th>
-          <th>Rol</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach ($dataUsers as $dataUser): ?>
-          <tr>
-            <td>
-              <?php echo $dataUser->Nombres ?>
-            </td>
-            <td>
-              <?php echo $dataUser->Apellidos ?>
-            </td>
-            <td>
-              <?php echo $dataUser->documento ?>
-            </td>
-            <td>
-              <?php echo $dataUser->correo ?>
-            </td>
-            <td>
-              <?php echo $dataUser->Rol ?>
-            </td>
-            <td>
-              <a class="btn btn-tbl-delete btn-xs"
-                href="?controller=Home&method=editUser&id=<?php echo $dataUser->idPersona ?>"><i
-                  class="fa fa-edit "></i></a>
-              <a class="btn btn-tbl-eliminar-ajax btn-xs"
-                href="?controller=Home&method=delete&id=<?php echo $dataUser->idPersona ?>"><i
-                  class="fas fa-trash-alt"></i></a>
-            </td>
-          </tr>
-        <?php endforeach ?>
-      </tbody>
-    </table>
-  </div>
-</div>
-<!-- Fin de contenido -->
-</div>
+  <!-- JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-<script>
-  $(document).ready(function () {
-    $('#userTable').DataTable();
-
-    $('.btn-tbl-eliminar-ajax').click(function (event) {
-      event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-      var deleteUrl = $(this).attr('href'); // Obtiene la URL de eliminación del atributo href del enlace
-
-      Swal.fire({
-        title: '¿Estás seguro de que quieres eliminar?',
-        text: "No podrás revertir este cambio.",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, quiero eliminarlo.'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // Realizar la solicitud AJAX al servidor
-          $.ajax({
-            url: deleteUrl,
-            type: 'POST',
-            success: function (response) {
-              Swal.fire('Eliminado!', 'El usuario fue eliminado del sistema', 'success');
-              location.reload();
-            },
-          });
+  <script>
+    $(document).ready(function () {
+      $('#userTable').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+          {
+            extend: 'copy',
+            text: 'Copiar',
+          },
+          {
+            extend: 'csv',
+            text: 'CSV',
+          },
+          {
+            extend: 'excel',
+            text: 'Excel',
+          },
+          {
+            extend: 'pdf',
+            text: 'PDF',
+          },
+          {
+            extend: 'print',
+            text: 'Imprimir',
+          }
+        ],
+        language: {
+          buttons: {
+            copyTitle: 'Copiado al portapapeles',
+            copySuccess: {
+              _: '%d filas copiadas',
+              1: '1 fila copiada'
+            }
+          }
         }
       });
-    });
-  });
-</script>
-</body>
 
+
+      $('.btn-tbl-eliminar-ajax').click(function (event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        var deleteUrl = $(this).attr('href'); // Obtiene la URL de eliminación del atributo href del enlace
+
+        Swal.fire({
+          title: '¿Estás seguro de que quieres eliminar?',
+          text: "No podrás revertir este cambio.",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Si, quiero eliminarlo.'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Realizar la solicitud AJAX al servidor
+            $.ajax({
+              url: deleteUrl,
+              type: 'POST',
+              success: function (response) {
+                Swal.fire('Eliminado!', 'El usuario fue eliminado del sistema', 'success');
+                location.reload();
+              },
+            });
+          }
+        });
+      });
+    });
+  </script>
+</body>
 </html>
